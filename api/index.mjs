ecc3774d95a879b52b75706b8cd6fc4611ee2611
cyro-hub <cyrocyro2@gmail.com>
2024-04-25 09:57:08 +0300

    created the project setup

---
+++ api/index.mjs
@@ -0,0 +1,39 @@
+/** @format */
+
+import express from "express";
+// import dotenv from "dotenv";
+// import "./config/database/dbconfig.mjs";
+// import cookieParser from "cookie-parser";
+import path from "path";
+// import routes from "./routes/routes.mjs";
+// import bodyParser from "body-parser";
+
+// dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+// app.use("/api", routes);
+
+app.use("/api", (req, res) => {
+  return res.json({ msg: "this is a test message" });
+});
+
+const __dirname = path.resolve();
+app.use(express.static("dist"));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
